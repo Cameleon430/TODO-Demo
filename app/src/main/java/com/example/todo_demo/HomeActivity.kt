d3985719410db088e60d3e176269fe50082801fb
Cameleon430 <Lav25602@gmail.com>
2022-09-02 01:20:06 +0300

    Made: transition from start screen, activity_home.xml, floating action button with toast, removed action bar.

--- app/src/main/java/com/example/todo_demo/HomeActivity.kt
+++ app/src/main/java/com/example/todo_demo/HomeActivity.kt
@@ -1,14 +1,21 @@
 package com.example.todo_demo
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val fab: View = findViewById(R.id.floating_button)
+
+        fab.setOnClickListener{
+            Toast.makeText(applicationContext, "Hey, you clicked it!", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
--- app/src/main/java/com/example/todo_demo/MainActivity.kt
+++ app/src/main/java/com/example/todo_demo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.todo_demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startScreen: View = findViewById(R.id.start_screen)
+
+        startScreen.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

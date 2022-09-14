8ba728ba81eb223afbfcc981e9c5d7d4a5c4648c
Cameleon430 <Lav25602@gmail.com>
2022-09-14 17:20:20 +0300

    Saving state on task screen

--- app/src/main/java/com/example/todo_demo/TaskDetailActivity.kt
+++ app/src/main/java/com/example/todo_demo/TaskDetailActivity.kt
@@ -1,23 +1,40 @@
 package com.example.todo_demo
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class TaskDetailActivity : AppCompatActivity(){
 
+    private lateinit var taskTitleEditText: EditText
+    private lateinit var taskDescriptionEditText: EditText
+
     companion object{
+        const val KEY_TITLE = "TASK_TITLE"
+        const val KEY_DESCRIPTION = "TASK_DESCRIPTION"
         const val EXTRA_NAME = "TASK_NUM"
-        const val DEFAULT_VAL = -1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
 
-        val taskTitleTextView: TextView = findViewById(R.id.taskTitleTextView)
-        val taskTitleText: String = "Task title " + intent.getIntExtra(EXTRA_NAME, DEFAULT_VAL).toString()
-
-        taskTitleTextView.text = taskTitleText
+        taskTitleEditText = findViewById(R.id.taskTitleEditText)
+        taskDescriptionEditText = findViewById(R.id.taskDescriptionEditText)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(KEY_TITLE, taskTitleEditText.text.toString())
+        outState.putString(KEY_DESCRIPTION, taskDescriptionEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        taskTitleEditText.setText(savedInstanceState.getString(KEY_TITLE))
+        taskDescriptionEditText.setText(savedInstanceState.getString(KEY_DESCRIPTION))
+    }
+
 }

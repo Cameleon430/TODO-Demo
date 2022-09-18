a5964b22525927e1b394e8582de4d72087816b78
Cameleon430 <Lav25602@gmail.com>
2022-09-18 16:01:07 +0300

    Saving and restore data

--- app/src/main/java/com/example/todo_demo/TaskDetailActivity.kt
+++ app/src/main/java/com/example/todo_demo/TaskDetailActivity.kt
@@ -1,18 +1,32 @@
 package com.example.todo_demo
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.properties.Delegates
 
 class TaskDetailActivity : AppCompatActivity(){
 
     private lateinit var taskTitleEditText: EditText
     private lateinit var taskDescriptionEditText: EditText
+    private lateinit var saveTaskButton: FloatingActionButton
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+    private var taskID by Delegates.notNull<Int>()
+
 
     companion object{
-        const val KEY_TITLE = "TASK_TITLE"
-        const val KEY_DESCRIPTION = "TASK_DESCRIPTION"
-        const val EXTRA_NAME = "TASK_NUM"
+        const val STORAGE_NAME_KEY = "MY_TASKS"
+        const val TASK_TITLE_KEY = "TASK_TITLE"
+        const val TASK_DESCRIPTION_KEY = "TASKS_DESCRIPTION"
+        const val TASK_ID_KEY = "TASK_ID"
+        const val TASK_ID_VAL = 1
+
+        const val EXTRA_NAME_KEY = "TASK_NUM"
+        const val DEFAULT_VAL = -1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,20 +35,55 @@ class TaskDetailActivity : AppCompatActivity(){
 
         taskTitleEditText = findViewById(R.id.taskTitleEditText)
         taskDescriptionEditText = findViewById(R.id.taskDescriptionEditText)
+        saveTaskButton = findViewById(R.id.saveTaskButton)
+
+        sharedPreferences = getSharedPreferences(STORAGE_NAME_KEY, MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+        taskID = intent.getIntExtra(EXTRA_NAME_KEY, DEFAULT_VAL)
+
+        saveTaskButton.setOnClickListener{
+            saveData()
+        }
+
+        setData()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
+    private fun setData(){
+        val savedID = sharedPreferences.getInt(TASK_ID_KEY, DEFAULT_VAL)
 
-        outState.putString(KEY_TITLE, taskTitleEditText.text.toString())
-        outState.putString(KEY_DESCRIPTION, taskDescriptionEditText.text.toString())
+        if(savedID == taskID) {
+            taskTitleEditText.setText(sharedPreferences.getString(TASK_TITLE_KEY, ""))
+            taskDescriptionEditText.setText(sharedPreferences.getString(TASK_DESCRIPTION_KEY, ""))
+        }
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
+    private fun saveData(){
+        editor.putString(TASK_TITLE_KEY ,taskTitleEditText.text.toString())
+        editor.putString(TASK_DESCRIPTION_KEY, taskDescriptionEditText.text.toString())
+        editor.putInt(TASK_ID_KEY, TASK_ID_VAL)
+        editor.apply()
 
-        taskTitleEditText.setText(savedInstanceState.getString(KEY_TITLE))
-        taskDescriptionEditText.setText(savedInstanceState.getString(KEY_DESCRIPTION))
+        finish()
+    }
+
+    override fun onBackPressed() {
+        if(hasUnsavedData()){
+            showUnsavedDataMessage()
+        }else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun hasUnsavedData(): Boolean {
+        val taskTitleText: String = taskTitleEditText.text.toString()
+        val taskDescriptionText: String = taskDescriptionEditText.text.toString()
+
+        return taskTitleText != sharedPreferences.getString(TASK_TITLE_KEY, "") ||
+            taskDescriptionText != sharedPreferences.getString(TASK_DESCRIPTION_KEY, "")
+    }
+
+    private fun showUnsavedDataMessage(){
+        Toast.makeText(applicationContext, "You have unsaved changes!", Toast.LENGTH_SHORT).show()
     }
 
 }
